7a71bf92df03f6a747c1ef2111d84b16f27eccba
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-03-25 10:55:43 +0200

    Lint azure

--- packages/azure/src/features/suggestions/suggestions-get.ts
+++ packages/azure/src/features/suggestions/suggestions-get.ts
@@ -1,13 +1,14 @@
-import { app, HttpResponseInit } from "@azure/functions";
-import { suggestionsGetRepository } from "./suggestions-repository";
+import type {HttpResponseInit} from '@azure/functions';
+import {app} from '@azure/functions';
+import {suggestionsGetRepository} from './suggestions-repository';
 
 export const suggestionsGet = async (): Promise<HttpResponseInit> => {
   const result = await suggestionsGetRepository();
-  return { body: result };
+  return {body: result};
 };
 
-app.http("suggestionsGet", {
-  methods: ["GET"],
-  authLevel: "anonymous",
+app.http('suggestionsGet', {
+  methods: ['GET'],
+  authLevel: 'anonymous',
   handler: suggestionsGet,
 });
--- packages/azure/src/features/suggestions/suggestions-repository.ts
+++ packages/azure/src/features/suggestions/suggestions-repository.ts
@@ -1,16 +1,16 @@
-import { openai } from "../../lib/openai";
+import {openai} from '../../lib/openai';
 
 export const suggestionsGetRepository = async (): Promise<string> => {
   try {
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
+      model: 'text-davinci-003',
       prompt:
-        "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+        'Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.',
       max_tokens: 100,
       temperature: 0.8,
     });
     return response.data.choices[0].text;
   } catch (error) {
-    return error;
+    return JSON.stringify(error);
   }
 };
